3d2be0b0dc019350fdc6599328b4b3c2f2cc7a16
Yanzu-zz <2273359360@qq.com>
2023-09-14 19:27:09 +0800

    adding ptb data set

--- Deep Learngin/Deep Learning Advanced/Chapter2/py_word.py
+++ Deep Learngin/Deep Learning Advanced/Chapter2/py_word.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import numpy as np
 
 
@@ -91,6 +92,29 @@ def create_co_matrix(corus, vocab_size, window_size=1):
     return co_matrix
 
 
+# Positive Pointwise Mutual Information
+# 也就是让概论最小为0
+def ppmi(C, verbose=False, eps=1e-8):
+    M = np.zeros_like(C, dtype=np.float32)
+    N = np.sum(C)
+    S = np.sum(C, axis=0)
+    total = C.shape[0] * C.shape[1]
+    cnt = 0
+
+    # 暴力计算 字符串 的 PMI 值
+    for i in range(C.shape[0]):
+        for j in range(C.shape[1]):
+            pmi = np.log2(C[i, j] * N / (S[j] * S[i]) + eps)
+            M[i, j] = max(0, pmi)
+
+            if verbose:
+                cnt += 1
+                if cnt % (total // 100 + 1) == 0:
+                    print('%.1f%% done' % (100 * cnt / total))
+
+    return M
+
+
 text = 'You say goodbye and I say hello.'
 corpus, word_to_id, id_to_word = preprocess(text)
 # print(corpus)
@@ -108,3 +132,22 @@ c1 = C[word_to_id['i']]
 print(cos_similarity(c0, c1))
 
 most_similar('you', word_to_id, id_to_word, C, top=5)
+
+
+# 转换共现矩阵为 PPMI 矩阵
+W=ppmi(C)
+np.set_printoptions(precision=3)
+print('covariance matrix')
+print(C)
+print('-'*50)
+print('PPMI')
+print(W)
+
+# 基于 SVD（Singular Value Decomposition） 的降维
+U,S,V=np.linalg.svd(W)
+
+# 使用对应的前 n 个数据就进行了降维
+for word, word_id in word_to_id.items():
+    plt.annotate(word,(U[word_id,0],U[word_id,1]))
+plt.scatter(U[:,0],U[:,1],alpha=0.5)
+plt.show()
